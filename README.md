03f5c3e1fe1d34954000a29e9ed670334fbacba9
Andrew Red <andrew@reddikh.com>
2020-11-05 22:51:23 +0300

    - added docs - fixed action for userId

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # medium-post-markdown
-Github Action for posting markdown based posts to medium.com
+Github Action for posting a markdown post to medium.com
+
+## Inputs
+
+### `app_id`
+
+**Required** Application id. Create one on the [page](https://medium.com/me/applications).
+
+### `app_secret`
+
+**Required** Application secret.
+
+### `access_token`
+
+**Required** User's access token. Create one on the [page](https://medium.com/me/settings).
+
+### `markdown`
+
+**Required** The markdown content of the article.
+
+### `post_url`
+
+Canonical post url if the article was published elsewhere.
+
+### `post_status`
+
+Post's status. Valid values are `draft`, `public`, or `unlisted`. Default `draft`. 
+
+### `post_license`
+
+Post's license. Valid values are `all-rights-reserved`, `cc-40-by`, `cc-40-by-sa`, `cc-40-by-nd`, `cc-40-by-nc`, `cc-40-by-nc-nd`, `cc-40-by-nc-sa`, `cc-40-zero`, `public-domain`. Default `all-rights-reserved`.
+
+## Outputs
+
+### `id`
+
+ID of the created post.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1.1
+with:
+  who-to-greet: 'Mona the Octocat'
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const {
   INPUT_APP_SECRET,
   INPUT_ACCESS_TOKEN,
 
-  INPUT_USER_ID,
   INPUT_MARKDOWN,
   INPUT_POST_URL,
   INPUT_POST_STATUS = sdk.PostPublishStatus.DRAFT,
@@ -16,10 +15,22 @@ const {
 const client = new sdk.MediumClient({ clientId: INPUT_APP_ID, clientSecret: INPUT_APP_SECRET });
 client.setAccessToken(INPUT_ACCESS_TOKEN);
 
-const createPost = async (title, tags, markdown) => {
+const getUser = async () => {
+  return new Promise((resolve, reject) => {
+    client.getUser((err, user) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(user);
+    });
+  });
+};
+
+const createPost = async (userId, title, tags, markdown) => {
   return new Promise((resolve, reject) => {
     client.createPost({
-      userId: INPUT_USER_ID,
+      userId,
       title: title,
       tags: tags,
       canonicalUrl: INPUT_POST_URL,
@@ -39,13 +50,14 @@ const createPost = async (title, tags, markdown) => {
 
 (async () => {
   try {
+    const { id } = await getUser();
     const { meta, markdown } = md(INPUT_MARKDOWN);
     const { title, tags = [] } = meta;
-    const post = await createPost(title, tags, markdown);
+    const post = await createPost(id, title, tags, markdown);
     console.log(`::set-output name=id::${post.id}`);
     process.exit(0);
   } catch (err) {
-    console.log(`::error ::${err.stack}`);
+    console.log(`::error ::${err.message}`);
     process.exit(1);
   }
 })();
